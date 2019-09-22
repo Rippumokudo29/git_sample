8e70184d1568c64765e0516d23e6177802a64c86
Rippumokudo29 <Mutukihihi29@gmail.com>
2019-09-23 02:34:15 +0900

    create calc.h

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+int sum(int, int);
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "This is master" <<std::endl;
+    int a = 1; int b =  9;
+    std::cout << sum(a,b) << std::endl;
     return 0;
+}
+
+int sum(int a, int b)
+{
+    return a + b;
 }
